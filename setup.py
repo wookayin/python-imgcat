dc1ac1795e9931e89bcef902c6bccd87a6a292d6
Hiroshi Toyama <toyama0919@gmail.com>
2020-03-07 11:11:21 +0900

    fix python2.7 require future for pytorch.

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ install_requires = [
 tests_requires = [
     'pytest<5.0',
     'numpy',
+    'torch',
     'torchvision',
 ]
 if sys.version_info >= (3, 6):
@@ -39,6 +40,10 @@ elif sys.version_info >= (3, 5):
     tests_requires += ['matplotlib~=3.0.3', 'Pillow']
 else:  # <= Python 3.4
     tests_requires += ['matplotlib<3.0', 'Pillow<6.0']
+
+# pytorch: python 2.7 require future
+if sys.version_info < (3, 0):
+    tests_requires += ['future']
 
 __version__ = read_version()
 
