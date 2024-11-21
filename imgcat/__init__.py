814e3429f7e0510ed0124e641ef04b5bce2ad64d
Jongwook Choi <wookayin@gmail.com>
2024-11-20 21:22:21 -0500

    Release version v0.6.0

--- imgcat/__init__.py
+++ imgcat/__init__.py
@@ -2,7 +2,7 @@
 The imgcat module
 """
 
-__version__ = '0.6.0.dev0'
+__version__ = '0.6.0'
 
 
 from .imgcat import imgcat as imgcat
--- setup.py
+++ setup.py
@@ -60,6 +60,7 @@ setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
     ],
     packages=['imgcat'],
     python_requires='>=3.6',
